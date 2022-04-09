51553055ba310b9942f40dd4849a9df17ab7c4b8
Myakizh <xoxlovvalentin5@gmail.com>
2022-04-10 00:39:18 +0300

    added pipelines

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Scrapper
 
-Just the first step
+Just the first step.
 Spider take information from Blizzard's site
-(but xpath recvests work with string(looking a little strange))
+(but xpath recvests work with string(looking a little strange)).
+Scrapper write down info into database.
+
 How to run:
 1. ".//env/scripts/activate"
 2. "pip install scrapy"
-3. "scrapy runspider blizzard_spider.py -o info.csv"
+3. "scrapy runspider blizzard_spider.py <-o info.csv>"
--- blizzard_scraper/blizzard_scraper/pipelines.py
+++ blizzard_scraper/blizzard_scraper/pipelines.py
@@ -6,8 +6,24 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import sqlite3
 
 class BlizzardScraperPipeline:
+    def __init__(self):
+        self.con = sqlite3.connect('base.db')
+        self.cur = self.con.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS winners(
+            name TEXT,
+            title TEXT,
+            university TEXT,
+            image TEXT
+        )''')
+
     def process_item(self, item, spider):
+        self.cur.execute('''INSERT OR IGNORE INTO winners VALUES(?, ?, ?, ?)''', 
+            (item['person'], item['title'], item['university'], item['img_url']))
+        self.con.commit()
         return item
