b919608fbfffa37a8d63794866f8c0dae839f146
Myakizh <xoxlovvalentin5@gmail.com>
2022-04-09 23:37:28 +0300

    working spider

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Scrapper
 
-just the first step
+Just the first step
+Spider take information from Blizzard's site
+(but xpath recvests work with string(looking a little strange))
+How to run:
+1. ".//env/scripts/activate"
+2. "pip install scrapy"
+3. "scrapy runspider blizzard_spider.py -o info.csv"
--- blizzard_scraper/blizzard_scraper/items.py
+++ blizzard_scraper/blizzard_scraper/items.py
@@ -4,9 +4,15 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst, MapCompose
 
+def strip(value) :
+    return value.strip()
 
-class BlizzardScraperItem(scrapy.Item):
+class BlizScraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    person = scrapy.Field(input_processor = MapCompose(strip), output_processor = TakeFirst())
+    university = scrapy.Field(input_processor = MapCompose(), output_processor = TakeFirst())
+    title = scrapy.Field(input_processor = MapCompose(strip), output_processor = TakeFirst())
+    img_url = scrapy.Field(input_processor = MapCompose(), output_processor = TakeFirst())
